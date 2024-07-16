a5746162d2b2c658cccc423446859fb69bb057de
SangEun Lee <dev.max@icloud.com>
2024-07-16 14:07:21 +0900

    Feat: Mysql connection ip config

--- app/core/database.py
+++ app/core/database.py
@@ -1,6 +1,6 @@
 import pymysql
 
 async def Connect():
-    connection = pymysql.connect(host="172.26.15.219", user="root", passwd="", db="ggamsi", charset="utf8")
+    connection = pymysql.connect(host="localhost", user="root", passwd="", db="ggamsi", charset="utf8")
     cursor = connection.cursor()
     return connection, cursor
