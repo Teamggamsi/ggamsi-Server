bf9c0b64597fd8e7062d0a9324392f18f6d03cf0
SangEun Lee <dev.max@icloud.com>
2024-07-16 14:16:08 +0900

    Update: mysql ip adress

--- app/core/database.py
+++ app/core/database.py
@@ -1,6 +1,6 @@
 import pymysql
 
 async def Connect():
-    connection = pymysql.connect(host="localhost", user="root", passwd="", db="ggamsi", charset="utf8")
+    connection = pymysql.connect(host="43.201.116.75", user="root", passwd="", db="ggamsi", charset="utf8")
     cursor = connection.cursor()
     return connection, cursor
