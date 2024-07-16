e106b14fb8f45304493d19578da7df32ee7732d6
SangEun Lee <dev.max@icloud.com>
2024-07-16 09:22:16 +0900

    Remove: Not necessary codes

--- api/main.py
+++ api/main.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter
 
-from api.routes import login,rank
+from api.routes import login
 
 api_router = APIRouter()
 api_router.include_router(login.router, tags=["Auth"])
-api_router.include_router(rank.router, tags=["Rank"])
