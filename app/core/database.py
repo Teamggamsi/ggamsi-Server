b5f0dc730c600ab5e66dff0f1fac181bd1a8a4a1
SangEun Lee <dev.max@icloud.com>
2024-07-16 14:27:48 +0900

    Feat: Update mysql port pw

--- app/core/database.py
+++ app/core/database.py
@@ -1,6 +1,6 @@
 import pymysql
 
 async def Connect():
-    connection = pymysql.connect(host="43.201.116.75", user="root", passwd="", db="ggamsi", charset="utf8")
+    connection = pymysql.connect(host="43.201.116.75", port=3306, user="root", passwd="1234", db="ggamsi", charset="utf8")
     cursor = connection.cursor()
     return connection, cursor
