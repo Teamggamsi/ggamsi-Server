91adb5618facb4b94d02163c7d70dd0499b6df01
SangEun Lee <dev.max@icloud.com>
2024-07-17 11:59:22 +0900

    Feat: Add User tokens

--- app/api/routes/product.py
+++ app/api/routes/product.py
@@ -18,7 +18,7 @@ async def 상품_글_작성하기(params: postProductParam):
     if (len(data["token"]) > 0 and token):
         try:
             connection, cursor = await Connect()
-            cursor.execute("INSERT INTO products(title, content, price, tag, image, author) VALUES(%s, %s, %s, %s, %s, %s);", (data["title"], data["content"], data["price"], data["category"], data["img"], token))
+            cursor.execute("INSERT INTO products(title, content, delivery, price, tag, image, author) VALUES(%s, %s, %s, %s, %s, %s, %s);", (data["title"], data["content"], data["delivery"], data["price"], data["category"], data["img"], token))
             connection.commit()
             connection.close()
             return {
@@ -35,108 +35,64 @@ async def 상품_글_작성하기(params: postProductParam):
 
 
 @router.post("/products")
-async def 상품_목록_불러오기(params: getParam):
-    data = dict(params)
-    token = await validateToken(data["token"])
-    if (len(data["token"]) > 0 and token):
-        try:
-            connection, cursor = await Connect()
-            cursor.execute("SELECT * FROM products;")
-            rows = cursor.fetchall()
-            productData = {}
-            for i in rows:
-                productData[i[0]] = {
-                    "id": i[0],
-                    "title": i[1],
-                    "content": i[2],
-                    "price": i[3],
-                    "category": i[4],
-                    "image": i[5],
-                    "author": i[6],
-                }
-            connection.close()
-            return {
-                "success": True,
-                "data": productData
-            }
-        except Exception as e:
-            print(e)
-            return {
-                "success": False,
-                "message": "서버에서 오류가 발생하였습니다."
-            }
-    else:
-        return HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="토큰이 유호하지 않습니다.")
-    
+async def 상품_목록_불러오기():
+    connection, cursor = await Connect()
+    cursor.execute("SELECT * FROM products;")
+    rows = cursor.fetchall()
+    productData = {}
+    for i in rows:
+        productData[i[0]] = {
+            "id": i[0],
+            "title": i[1],
+            "content": i[2],
+            "price": i[3],
+            "category": i[4],
+            "image": i[5],
+            "author": i[6],
+        }
+    connection.close()
+    return {
+        "success": True,
+        "data": productData
+    }
 
 @router.post("/products/{category}")
-async def 카테고리로_글_가져오기(category:str, params: getParam):
-    data = dict(params)
-    token = await validateToken(data["token"])
-    if (len(data["token"]) > 0 and token):
-        try:
-            connection, cursor = await Connect()
-            cursor.execute("SELECT * FROM products WHERE tag = %s;",(category))
-            rows = cursor.fetchall()
-            productData = []
-            for i in rows:
-                productData.append({
-                    "id": i[0],
-                    "title": i[1],
-                    "content": i[2],
-                    "price": i[3],
-                    "category": i[4],
-                    "image": i[5],
-                    "author": i[6],
-                })
-            connection.close()
-            return {
-                "success": True,
-                "data": productData
-            }
-        except Exception as e:
-            print(e)
-            return {
-                "success": False,
-                "message": "서버에서 오류가 발생하였습니다."
-            }
-    else:
-        return HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="토큰이 유호하지 않습니다.")    
+async def 카테고리로_글_가져오기(category:str):
+    connection, cursor = await Connect()
+    cursor.execute("SELECT * FROM products WHERE tag = %s;",(category))
+    rows = cursor.fetchall()
+    productData = []
+    for i in rows:
+        productData.append({
+            "id": i[0],
+            "title": i[1],
+            "content": i[2],
+            "price": i[3],
+            "category": i[4],
+            "image": i[5],
+            "author": i[6],
+        })
+    connection.close()
+    return {
+        "success": True,
+        "data": productData
+    }
 
 @router.post("/product/{id}")
-async def 상품_아이디로_목록_불러오기(id: str, params: getDetailParam):
-    data = dict(params)
-    token = await validateToken(data["token"])
-    if (len(data["token"]) > 0 and token):
-        try:
-            connection, cursor = await Connect()
-            cursor.execute("SELECT * FROM products WHERE id = %s;", (id))
-            rows = cursor.fetchone()
-            cursor.execute("SELECT * FROM likes WHERE article = %s;", (id))
-            likeRows = cursor.fetchall()
-            cursor.execute("SELECT * FROM likes WHERE article = %s and author = %s;", (id, token))
-            likeRow = cursor.fetchone()
-            connection.close()
-            return {
-                "success": True,
-                "data": {
-                    "id": rows[0],
-                    "title": rows[1],
-                    "content": rows[2],
-                    "price": rows[3],
-                    "category": rows[4],
-                    "image": rows[5],
-                    "is_liked": (not likeRow == None),
-                    "likes": len(likeRows),
-                    "author": rows[6],
-                }
-            }
-        except Exception as e:
-            print(e)
-            return {
-                "success": False,
-                "message": "서버에서 오류가 발생하였습니다."
-            }
-    else:
-        return HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="토큰이 유호하지 않습니다.")
-
+async def 상품_아이디로_목록_불러오기(id: str):
+    connection, cursor = await Connect()
+    cursor.execute("SELECT * FROM products WHERE id = %s;", (id))
+    rows = cursor.fetchone()
+    connection.close()
+    return {
+        "success": True,
+        "data": {
+            "id": rows[0],
+            "title": rows[1],
+            "content": rows[2],
+            "price": rows[3],
+            "category": rows[4],
+            "image": rows[5],
+            "author": rows[6],
+        }
+    }
--- app/schemas/product.py
+++ app/schemas/product.py
@@ -3,13 +3,8 @@ from pydantic import BaseModel
 class postProductParam(BaseModel):
     token: str
     title: str
+    delivery:int
     price:int
     content: str
     img: str
     category: str
-
-class getParam(BaseModel):
-    token: str
-
-class getDetailParam(BaseModel):
-    token: str
